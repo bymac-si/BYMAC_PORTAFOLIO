cf8cf0c42282a46692e6ff8cdbdb7f7829278ae2
Marcos Castro Abarca <61596615+bymac-si@users.noreply.github.com>
2025-12-03 20:14:08 -0300

    Sprint 1: Maquetación Buscador de Recetas

---
+++ gourmet-go/assets/js/app.js
@@ -0,0 +1,5 @@
+// app.js - F-M3 Proyecto Integrador Sprint 1
+// Por ahora este archivo es solo un placeholder.
+// La lógica de búsqueda y consumo de APIs se implementará en el Sprint 2. 
+
+// console.log("Gourmet Go · Buscador de Recetas · Sprint 1 listo.");
